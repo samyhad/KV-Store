e4c8d840f99296cf2a7c47d6f8c8473edb9a6c18
Samira Haddad <60987282+samyhad@users.noreply.github.com>
2023-07-12 20:13:23 -0300

    Update ThreadPUT.java

--- ThreadPUT.java
+++ ThreadPUT.java
@@ -1,1 +1,25 @@
+public class ThreadPUT extends Thread{
 
+    private Socket no = null;
+
+    public ThreadPUT(Socket node){
+        no = node;
+    }
+
+    public void run(){
+        try{
+            InputStreamReader is =  new InputStreamReader(no.getInputStream());
+            BufferedReader reader = new BufferedReader(is);
+            OutputStream os = no.getOutputStream();
+            DataOutputStream writer = new DataOutputStream(os);
+            String texto = reader.readLine();
+            writer.writeBytes(texto.toUpperCase() + '\n');
+
+        }catch(Exception e){
+            //
+        }
+
+    }
+    
+    
+}
