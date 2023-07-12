a60bfb3e087e4b7b58e3ebdbb02142e587897a43
Samira Haddad <60987282+samyhad@users.noreply.github.com>
2023-07-12 20:11:21 -0300

    Update Server.java

--- Server.java
+++ Server.java
@@ -19,20 +19,33 @@ public class Server {
 
     public static void main(String[] args) throws Exception{
 
-      Scanner scanner = new Scanner(System.in);
-      //scanner.nextLine();
-      //System.out.println("Qual o IP desse peer?");
-      //address_str = scanner.nextLine();
-      //System.out.println("Qual a porta desse peer?");
-      //porta = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        //scanner.nextLine();
+        //System.out.println("Qual o IP desse peer?");
+        //address_str = scanner.nextLine();
+        //System.out.println("Qual a porta desse peer?");
+        //porta = scanner.nextInt();
+        
+        ServerSocket serverSocket = new ServerSocket(10099);
+        InetAddress address = serverSocket.getInetAddress();
 
-      InetSocketAddress endereco = new InetSocketAddress(address_str, porta);
-      ServerSocket serverSocket = new ServerSocket();
-      serverSocket.bind(endereco);
-      //ServerSocket serverSocket = new ServerSocket(0);
-      //porta = serverSocket.getLocalPort();
-      InetAddress address = serverSocket.getInetAddress();
+        //10097, 10098 e 10099
 
+        while(true) {
+            //socket nó é o socket conectivo
+            // socket nó terá um porta designada pelo SP - entre 1-24 e 65535
+            //accept(): O método accept() escuta uma conexão e aceita se alguma for encontrada. 
+            //O accept() bloqueia todo o restante até que uma conexão seja feita, 
+            //ele fica em espera aguardando que alguém conecte. Quando alguma conexão é aceita ele 
+            //retorna um objeto Socket, que veremos mais à frente.
+            System.out.println("Esperando conexão com cliente");
+            Socket no = serverSocket.accept(); //bloqueante - fica nesse ponto esperando ação
+            System.out.println("Conexão aceita");
+            
+            //thread para atender novo nó
+            ThreadPUT thread = new ThreadPUT(no);
+            thread.start(); //executa a thread -> chamada ao método run()
+        }
       
     }
 }
