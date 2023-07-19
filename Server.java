afbd24697faa2eb68647692e20e4c77250be8fea
Samira Haddad <samira.had.2000@gmail.com>
2023-07-19 20:56:25 -0300

    Alterações

--- Server.java
+++ Server.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -18,68 +21,89 @@ import java.net.Socket;
 public class Server {
 
     private Hashtable<Integer, String> hashTableKV;
+    public static ArrayList<Peer> servers = new ArrayList<>();
+    
     private int port;
     private String ipAddress;
     private Boolean isLeader;
-    private InetSocketAddress LeaderAddress; 
-
-    private static void setPort (int port){
+    private static Peer peer;
+    
+    public int getPort() {
+        return port;
+    }
+    public void setPort(int port) {
         this.port = port;
     }
-
-    private static int getPort (
-        return this.port;
-    ) 
-
-    private static void setIpAddress (int ipAddress){
+    public String getIpAddress() {
+        return ipAddress;
+    }
+    public void setIpAddress(String ipAddress) {
         this.ipAddress = ipAddress;
     }
+    public Boolean getIsLeader() {
+        return isLeader;
+    }
+    public void setIsLeader(Boolean isLeader) {
+        this.isLeader = isLeader;
+    }
+    
 
-    private static int getIpAddress (
-        return this.ipAddress;
-    ) 
+
 
     public static void main(String[] args) throws Exception{
+        
+        Server server = new Server();
 
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
         System.out.println("Qual o IP desse servidor?");
-        setIpAddress(scanner.nextLine());
+        server.setIpAddress(scanner.nextLine());
         System.out.println("Qual a porta desse servidor?");
-        setPort(scanner.nextInt());
+        server.setPort(scanner.nextInt());
         
-        InetSocketAddress endereco = new InetSocketAddress(getIpAddress(), getPort());
+        InetSocketAddress endereco = new InetSocketAddress(server.getIpAddress(), server.getPort());
         
+        
+
         System.out.println("Esse servidor será o servidor líder? (S|N)");
         if(scanner.nextLine() == "S"){
-            
-        } else {
-            
+            server.isLeader = true;
+        } else if (scanner.nextLine() == "N"){
+            server.isLeader =  false;
+            System.out.println("Qual o IP do líder?");
+            server.setIpAddress(scanner.nextLine());
+            System.out.println("Qual a porta do líder?");
+            server.setPort(scanner.nextInt());
         }
 
-        
         ServerSocket serverSocket = new ServerSocket();
         serverSocket.bind(endereco);
+
+        peer = new Peer(serverSocket.getInetAddress(), server.port, server.getIsLeader());
+
+
+
+        connectWithLeader(peer);
+        
+
+
         
         //ServerSocket serverSocket = new ServerSocket(10099);
         
         //10097, 10098 e 10099
 
-        while(true) {
-            //socket nó é o socket conectivo
-            // socket nó terá um porta designada pelo SP - entre 1-24 e 65535
-            //accept(): O método accept() escuta uma conexão e aceita se alguma for encontrada. 
-            //O accept() bloqueia todo o restante até que uma conexão seja feita, 
-            //ele fica em espera aguardando que alguém conecte. Quando alguma conexão é aceita ele 
-            //retorna um objeto Socket, que veremos mais à frente.
-            System.out.println("Esperando conexão com cliente");
-            Socket no = serverSocket.accept(); //bloqueante - fica nesse ponto esperando ação
-            System.out.println("Conexão aceita");
-            
-            //thread para atender novo nó
-            ThreadPUT thread = new ThreadPUT(no);
-            thread.start(); //executa a thread -> chamada ao método run()
-        }
+        
       
+    }
+
+    public static void connectWithLeader(Peer peer) throws IOException{
+        // Criando o socket - conexão TCP entre os dois servidores
+        Socket socket_conn = new Socket(ipPeer, portaPeer);
+        socket_conn.close();
+    }
+
+    public static void connectWithOthersServer (String ipPeer, int portaPeer){
+        Socket socket_conn = new Socket(ipPeer, portaPeer);
+        socket_conn.close();
     }
 }
